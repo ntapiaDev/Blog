7d2f95c883c63b749af147366a8f3299738967b2
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-05-27 00:07:38 +0200

    Fixes

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,16 @@
 const fixedNav = document.querySelector('.fixed-nav');
-
 const sectionDestinations = document.querySelector('#destinations');
 
+if(sectionDestinations.getBoundingClientRect().y < 100) {
+    fixedNav.style.display = "flex";
+    fixedNav.style.opacity = (100 - sectionDestinations.getBoundingClientRect().y) / 100;
+}
 document.addEventListener('scroll', () => {
-    if(sectionDestinations.getBoundingClientRect().y < 85) {
+    if(sectionDestinations.getBoundingClientRect().y < 100) {
         fixedNav.style.display = "flex";
         fixedNav.style.opacity = (100 - sectionDestinations.getBoundingClientRect().y) / 100;
         console.log('down');
-    } else if(sectionDestinations.getBoundingClientRect().y > 85) {
+    } else if(sectionDestinations.getBoundingClientRect().y > 100) {
         fixedNav.style.display = "none";
         console.log('up');
     }
