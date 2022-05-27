14e4c6e3d4deb564ea11d8c60e53a995dcd966f3
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-05-27 11:14:06 +0200

    Form contact part1

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,7 @@ document.addEventListener('scroll', () => {
     if(sectionDestinations.getBoundingClientRect().y < 100) {
         fixedNav.style.display = "flex";
         fixedNav.style.opacity = (100 - sectionDestinations.getBoundingClientRect().y) / 100;
-        console.log('down');
-    } else if(sectionDestinations.getBoundingClientRect().y > 100) {
+    } else if(sectionDestinations.getBoundingClientRect().y > 100 && fixedNav.style.display !== "none") {
         fixedNav.style.display = "none";
-        console.log('up');
     }
 })
