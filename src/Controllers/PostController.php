51f9343c851be565f3b6c6f26ad3ee9130c3f29a
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-05-30 16:09:06 +0200

    PostController & model

--- src/Controllers/PostController.php
+++ src/Controllers/PostController.php
@@ -2,6 +2,7 @@
 namespace App\Controllers;
 
 use App\Core\Form;
+use App\Models\PostModel;
 
 class PostController extends Controller
 {
@@ -23,19 +24,52 @@ class PostController extends Controller
             exit;
         }
 
+        $title = isset($_POST['title']) ? strip_tags($_POST['title']) : '';
+        $hook = isset($_POST['hook']) ? strip_tags($_POST['hook']) : '';
+        $content = isset($_POST['content']) ? strip_tags($_POST['content']) : '';
+        if(!empty($_POST)) {
+            if(Form::validate($_POST, ['title', 'hook', 'category', 'content'])) {
+                // $title = strip_tags($_POST['title']);
+                // $hook = strip_tags($_POST['hook']);
+                // $content = strip_tags($_POST['content']);
+                $category = $_POST['category'];
+
+                $upload_dir = ROOT . '/public/uploads/';
+                $ext = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
+                $file_name = basename(md5(rand()) . '.' . $ext);
+                $upload_file = $upload_dir . $file_name;
+                $upload_img = '/uploads/' . $file_name;
+                move_uploaded_file($_FILES["image"]["tmp_name"], $upload_file);
+                
+                $post = new PostModel;
+                $post->setTitle($title)
+                    ->setHook($hook)
+                    ->setCategory($category)
+                    ->setImage($upload_img)
+                    ->setContent($content)
+                    ->setUser($_SESSION['user']['id'])
+                    ->setSlug(strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $title))));
+
+                // var_dump($post);
+                $post->create();
+                header('Location: /#destinations');
+                exit;
+            }
+        }
+
         $form = new Form;
 
-        $form->debutForm('post', '#', ['enctype' => 'multipart/formdata'])
+        $form->debutForm('post', '#', ['enctype' => 'multipart/form-data'])
             ->ajoutLabelFor('title', 'Titre :')
-            ->ajoutInput('text', 'title', ['id' => 'title', 'class' => '', 'placeholder' => 'Titre'])
+            ->ajoutInput('text', 'title', ['id' => 'title', 'class' => '', 'placeholder' => 'Titre', 'value' => $title])
             ->ajoutLabelFor('hook', 'Chapô :')
-            ->ajoutInput('text', 'hook', ['id' => 'hook', 'class' => '', 'placeholder' => 'Chapô'])
+            ->ajoutInput('text', 'hook', ['id' => 'hook', 'class' => '', 'placeholder' => 'Chapô', 'value' => $hook])
             ->ajoutLabelFor('category', 'Catégorie :')
-            ->ajoutSelect('category', ['forest' => 'Forêt', 'mountain' => 'Montagne', 'lake' => 'Lac', 'ocean' => 'Océan'], ['id' => 'category', 'class' => ''])
+            ->ajoutSelect('category', ['1' => 'Forêt', '2' => 'Montagne', '3' => 'Lac', '4' => 'Océan'], ['id' => 'category', 'class' => ''])
             ->ajoutLabelFor('image', 'Illustration :')
             ->ajoutInput('file', 'image', ['id' => 'image', 'class' => '',  'accept' => "image/jpeg, image/png"])
             ->ajoutLabelFor('content', 'Contenu :')
-            ->ajoutTextarea('content', '', ['id' => 'content', 'class' => ''])
+            ->ajoutTextarea('content', $content, ['id' => 'content', 'class' => ''])
             ->ajoutBouton('Créer un post', ['class' => ''])
             ->finForm();
 
---
+++ src/Models/PostModel.php
@@ -0,0 +1,222 @@
+<?php
+namespace App\Models;
+
+Class PostModel extends Model
+{
+    protected $id;
+    protected $title;
+    protected $hook;
+    protected $content;
+    protected $category;
+    protected $user;
+    protected $slug;
+    protected $created_at;
+    protected $updated_at;
+    protected $image;
+
+    public function __construct()
+    {
+        $class = str_replace(__NAMESPACE__.'\\', '', __CLASS__);
+        $this->table = strtolower(str_replace('Model', '', $class));
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of hook
+     */ 
+    public function getHook()
+    {
+        return $this->hook;
+    }
+
+    /**
+     * Set the value of hook
+     *
+     * @return  self
+     */ 
+    public function setHook($hook)
+    {
+        $this->hook = $hook;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of content
+     */ 
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the value of content
+     *
+     * @return  self
+     */ 
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of category
+     */ 
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set the value of category
+     *
+     * @return  self
+     */ 
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user
+     */ 
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the value of user
+     *
+     * @return  self
+     */ 
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of slug
+     */ 
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set the value of slug
+     *
+     * @return  self
+     */ 
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     */ 
+    public function getCreated_at()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set the value of created_at
+     *
+     * @return  self
+     */ 
+    public function setCreated_at($created_at)
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updated_at
+     */ 
+    public function getUpdated_at()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set the value of updated_at
+     *
+     * @return  self
+     */ 
+    public function setUpdated_at($updated_at)
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of image
+     */ 
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     *
+     * @return  self
+     */ 
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
